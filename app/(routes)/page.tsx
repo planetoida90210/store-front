781e3494c1128183f6ea71837a17a04e981521c5
Michał Bryk <galeriakoloru90210@gmail.com>
2023-08-25 11:22:44 +0200

    add billboard fetching from backend

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from "@/actions/getBillboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/Container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard();
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
